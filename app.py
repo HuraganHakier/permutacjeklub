0a1f3a2594f59c880c71aa724f156485a663d151
HuraganHakier <mikolajczyz0@gmail.com>
2025-06-20 12:13:31 +0200

    Add files via upload

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from itertools import permutations
+
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def index():
+    wynik = []
+    liczba_perm = 0
+    error = None
+
+    if request.method == "POST":
+        cyfry = request.form.get("cyfry")
+        if len(cyfry) != 6 or not cyfry.isdigit():
+            error = "Wprowadź dokładnie 6 cyfr."
+        else:
+            perms = sorted(set(permutations(cyfry)))
+            wynik = [''.join(p) for p in perms]
+            liczba_perm = len(wynik)
+
+    return render_template("index.html", wynik=wynik, liczba_perm=liczba_perm, error=error)
+
+if __name__ == "__main__":
+    app.run(debug=True)
